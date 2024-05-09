923640c2775e23904726102aacb47348f76ffc19
plug <plugajem@gmail.com>
2024-05-09 20:38:33 +0300

    it is my first commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello koljs
+          Hello kolja
         </p>
         <a
           className="App-link"
